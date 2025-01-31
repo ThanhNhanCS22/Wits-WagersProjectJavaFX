bbf262fb5dce036d1258237e49454a66353b4ecd
ThanhNhan <anhscientist2202@gmail.com>
2025-01-31 12:25:11 +0100

    Build Jar file that works on Mac ARM

---
+++ src/main/java/com/nhan/witsandwagers/Launcher.java
@@ -0,0 +1,10 @@
+package com.nhan.witsandwagers;
+
+import com.nhan.witsandwagers.Gui.GUI;
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(GUI.class, args);
+    }
+}
